b517bd18c581133b60bec76e9ba5c0b2e7de15d9
Kenta Adachi <kadachi1@uci.edu>
2023-03-02 14:24:56 -0800

    Added nonce and padding to encryption, Still need modification on nonce and key

--- host_tools/encryption.py
+++ host_tools/encryption.py
@@ -5,18 +5,30 @@
 # use command pip install cryptography in your virtual environment
 # documentation can be found here : https://pypi.org/project/pycrypto/
 
-# Fernet encrypt given data using AES
+import os
+
+# Fernet encrypt given data using AES-CBC
 # https://cryptography.io/en/latest/fernet/
 from cryptography.fernet import Fernet
 
+# key and nonce must be stored in a file
 key = Fernet.generate_key()
-
+nonce = os.urandom(4)
+ 
 class Encrypt:
     def __init__(self):
         self._fernet = Fernet(key)
     
-    def encrypt(self, plain_text):
+    def encrypt(self, message):
+        message_bytes = str.encode(message) + nonce # append 4 bytes nonce to message
+        pad = (16 - (len(message_bytes) % 16)) * '0' # padding with 0
+        plain_text = str.encode(pad) + message_bytes
         return self._fernet.encrypt(plain_text)
     
     def decrypt(self, cipher_text):
         return self._fernet.decrypt(cipher_text)
+
+    # fails to update nonce
+    def update_nonce(self):
+        global nonce
+        nonce = os.urandom(4)
